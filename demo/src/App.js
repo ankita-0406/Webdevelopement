183eb258fa6c12e26af27e76cbc7da49f3539b4e
Ankita <ankitabisht.rajput@gmail.com>
2020-04-04 17:54:34 +0530

    demo

---
+++ demo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Image  from "./Image";
+
+class App extends React.Component{
+        render(){
+            
+return (
+<div className = " ui container" style ={{marginTop :"10px"}}> 
+   <Image/>
+</div>
+);
+}
+        }
+        export default App;
